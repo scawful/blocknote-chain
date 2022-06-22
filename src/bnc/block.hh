189b667bbe3149b743cab7336c488484e9b015a1
scawful <justin.scofield@outlook.com>
2022-06-21 21:28:41 -0400

    Added block and wallet namespaces

---
+++ src/bnc/block.hh
@@ -0,0 +1,8 @@
+#ifndef BLOCKNOTE_BLOCK_H
+#define BLOCKNOTE_BLOCK_H
+
+namespace blocknote {
+namespace block {}
+}  // namespace blocknote
+
+#endif
---
+++ src/bnc/wallet.hh
@@ -0,0 +1,15 @@
+#ifndef BLOCKNOTE_WALLET_WALLET_H
+#define BLOCKNOTE_WALLET_WALLET_H
+
+namespace blocknote {
+namespace wallet {
+
+class Wallet {
+ public:
+  Wallet() = default;
+};
+
+}  // namespace wallet
+}  // namespace blocknote
+
+#endif  // BLOCKNOTE_WALLET_WALLET_H
