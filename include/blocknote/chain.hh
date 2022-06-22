f1e5cefd0123038a0e07f6880062dd36c43af0de
scawful <justin.scofield@outlook.com>
2022-06-21 22:44:08 -0400

    update namespaces

---
+++ include/blocknote/chain.hh
@@ -0,0 +1,11 @@
+#ifndef BLOCKNOTE_CHAIN_H
+#define BLOCKNOTE_CHAIN_H
+
+namespace blocknote {
+class Chain {
+  public:
+    Chain();
+};
+}  // namespace blocknote
+
+#endif
---
+++ include/blocknote/sha256.hh
@@ -0,0 +1,8 @@
+#ifndef BLOCKNOTE_SHA256_H
+#define BLOCKNOTE_SHA256_H
+
+namespace blocknote {
+namespace sha256 {}
+}  // namespace blocknote
+
+#endif
