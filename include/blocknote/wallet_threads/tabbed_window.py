42d1e4319672f4446bb47b445828db2465d4934f
scawful <justin.scofield@outlook.com>
2022-06-30 21:03:23 -0400

    added button and label class to tabbed window experimental wallet thread

--- include/blocknote/wallet_threads/tabbed_window.py
+++ include/blocknote/wallet_threads/tabbed_window.py
@@ -1,10 +1,27 @@
-from PySide6.QtWidgets import QTabWidget, QApplication, QWidget
+from PySide6.QtWidgets import QTabWidget, QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from PySide6.QtCore import Qt
 import sys
-from button_and_label import ButtonAndLabel
 
+class ButtonAndLabel(QWidget):
+
+    def __init__(self):
+        super(ButtonAndLabel, self).__init__()
+
+        self.button = QPushButton("button")
+        self.button.clicked.connect(self.buttonClicked)
+
+        self.label = QLabel("label: before clicked")
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.button)
+        layout.addWidget(self.label)
+
+        self.setLayout(layout)
+
+    def buttonClicked(self):
+        self.label.setText("label: after clicked")
 
 class TabbedWindow(QTabWidget):
-
     def __init__(self, parent=None):
         super(TabbedWindow, self).__init__(parent)
         widget1 = QWidget()
@@ -16,7 +33,6 @@ class TabbedWindow(QTabWidget):
 
 
 if __name__ == "__main__":
-
     app = QApplication(sys.argv)
     tabbedWindow = TabbedWindow()
     tabbedWindow.show()
