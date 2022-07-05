69096edc585763e2781f05f4ece7feffa35e8174
Ben Reardon <benmr92@icloud.com>
2022-07-05 16:11:55 -0400

    app template

---
+++ src/wallet/app_test.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from passlib.hash import sha256_crypt
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'blocknotelive22'
+app.config['MYSQL_DB'] = 'crypto'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.secret_key = 'secret123'
+    app.run(debug = True)
